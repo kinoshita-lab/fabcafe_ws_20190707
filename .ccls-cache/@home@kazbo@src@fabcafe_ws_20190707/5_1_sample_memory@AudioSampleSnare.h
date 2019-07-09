61518a32c8808b58abb3f006768a34ec3a86da5b
kaz saita <saita@kinoshita-lab.org>
2019-07-09 14:07:07 +0900

    AudioPlaySdWav does not work on Teensy 1.46, add modified version

--- .ccls-cache/@home@kazbo@src@fabcafe_ws_20190707/5_1_sample_memory@AudioSampleSnare.h
+++
@@ -1,2 +0,0 @@
-// Audio data converted from WAV file by wav2sketch
-extern const unsigned int AudioSampleSnare[2753];
--- 5_2_sample_sdcard/5_2_sample_sdcard.ino
+++ 5_2_sample_sdcard/5_2_sample_sdcard.ino
@@ -4,54 +4,42 @@
  #include <SD.h>
  #include <SerialFlash.h>
  #include <Bounce2.h> // // スイッチ用ライブラリ
- 
+ #include "play_sd_wav_modified.h"
  
  // GUItool: begin automatically generated code
- AudioPlaySdWav           playSdWav1;     //xy=153,263
+ AudioPlaySdWav_modified           playSdWav1;     //xy=153,263
  AudioOutputAnalogStereo  dacs1;          //xy=486,258
  AudioConnection          patchCord1(playSdWav1, 0, dacs1, 0);
  AudioConnection          patchCord2(playSdWav1, 1, dacs1, 1);
  // GUItool: end automatically generated code
- 
- // SDカードから読むのに必要な設定
- // Use these with the Teensy 3.5 & 3.6 SD card
- #define SDCARD_CS_PIN    BUILTIN_SDCARD
- #define SDCARD_MOSI_PIN  11  // not actually used
- #define SDCARD_SCK_PIN   13  // not actually used
  
  constexpr int SWITCH_1_PIN = 2; //2番ピンを使う
  Bounce sw1 = Bounce();
  
  void setup() {
      pinMode(SWITCH_1_PIN, INPUT_PULLUP);
+
+
+    if (!SD.begin(BUILTIN_SDCARD)) {
+    return;
+    }
  
-   // SDカードのセットアップ
-   SPI.setMOSI(SDCARD_MOSI_PIN);
-   SPI.setSCK(SDCARD_SCK_PIN);
-   if (!(SD.begin(SDCARD_CS_PIN))) {
-     // stop here, but print a message repetitively
-     while (1) {
-       Serial.println("Unable to access the SD card");
-       delay(500);
-     }
-   }
      sw1.attach(SWITCH_1_PIN);
      sw1.interval(10);
-     AudioMemory(800);
+     AudioMemory(100);
  
      AudioNoInterrupts();
- 
- 
+     
      AudioInterrupts();
-     Serial.begin(115200);
- 
  }
  
  void loop() {
+  
+  
    sw1.update(); // スイッチの状態をスキャン
  
    if (sw1.fell()) {
-     playSdWav1.play("MEOW2.WAV");
+     playSdWav1.play("LONG.WAV");
    }
  
  }
